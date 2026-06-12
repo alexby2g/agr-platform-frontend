bb0ec21025098d0525dda3122be28a288ded9765
Alexander <alexby2g@gmail.com>
2026-06-12 10:06:11 -0400

    Corregir API URL producción

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL || 'https://agr-platform-backend-2.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
